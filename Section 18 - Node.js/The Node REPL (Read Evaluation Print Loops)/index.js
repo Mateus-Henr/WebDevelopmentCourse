48049a0d0b03e5384abd60d0c8c4d7b262f18248
Mateus Henrique <mateush.study@gmail.com>
2022-03-18 12:52:02 -0300

    The basics on Node.js

---
+++ Section 18 - Node.js/The Node REPL (Read Evaluation Print Loops)/index.js
@@ -0,0 +1,10 @@
+/*  REPL (Read, Evaluation, Print, Loop)
+  It allows us to execute code in byte size chuncks. In other words, we can run several lines of codes one after the other.
+  To access the REPL you can just type "node" on the command line.
+  Also, by pressing TAB on an incomplete function name, you can see all the possible functions that you can use.
+
+  Exiting the REPL
+  ".exit" or "Ctrl + C" twice. However "Ctrl + C" is more overall used.
+*/
+
+// Ex: 3 + 9 => 12
---
+++ Section 18 - Node.js/The Power of the Command Line and How to Use Node/index.js
@@ -0,0 +1,4 @@
+// To run this file using your computer you can use the command: "node FILENAME".
+// Ex: "node index.js".
+
+console.log("Hello, World");
