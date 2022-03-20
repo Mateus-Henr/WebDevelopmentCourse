10dd0a6dc389faec9f73cca64a84a5c5747c20d0
Mateus Henrique <mateush.study@gmail.com>
2022-03-20 19:02:28 -0300

    Update on the calculator challenge

--- Section 19 - Express.js with Node.js/BMI Routing Challenge/calculator.js
+++ Section 19 - Express.js with Node.js/BMI Routing Challenge/calculator.js
@@ -11,16 +11,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => res.sendFile(__dirname + INDEX_HTML));
 
-app.get("/bmicalculator", () => res.sendFile(__dirname + CALCULATOR_HTML));
-
-app.post("CALCULATOR_HTML", (req, res) =>
+app.post("/", (req, res) =>
 {
-  var weight = Number(req.body.weight);
-  var height = Number(req.body.height);
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+
+  res.send(num1 + " + " + num2 + " = " + result);
+});
+
+
+app.get("/bmicalculator", (req, res) => res.sendFile(__dirname + CALCULATOR_HTML));
+
+app.post("/bmicalculator", (req, res) =>
+{
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
 
   var result = Math.round(weight / (height * height));
 
   res.send("Your BMI is " + result);
 });
+
 
 app.listen(port, () =>  console.log(`Calculator app listening on port ${port}`));
