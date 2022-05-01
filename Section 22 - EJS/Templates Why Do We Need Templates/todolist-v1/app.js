9ffac075bc0f1eb8a69e67c04703d2382c759add
Mateus Henrique <mateush.study@gmail.com>
2022-05-01 18:12:43 -0300

    Why to use templates

---
+++ Section 22 - EJS/Templates Why Do We Need Templates/todolist-v1/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) =>
+{
+  let today = new Date();
+  let dayNumber = today.getDay();
+
+  // Weekend
+  if (dayNumber === 6 || dayNumber === 7)
+  {
+    res.write("<h1>Yay it's a weekend!</h1>");
+  }
+  else
+  {
+    res.write("<p>It is not the weekend.</p>");
+    res.write("<h1>Boo! I have it work!</h1>");
+  }
+
+  // Notice that when the server sees the "send" method, it sees it as the final instruction.
+  // To send multiple pieces of data we use the "write" method and only at the end that we send it all
+  // using the "send" method without any parameters.
+  // You can see the "send" method as an email, and the "write" method as a messager.
+  // We can also send a file using "sendFile()".
+  // res.send();
+  // We can create an HTML template that changes depending on the logic used.
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, () =>
+{
+  console.log("Server started on port 3000.");
+});
