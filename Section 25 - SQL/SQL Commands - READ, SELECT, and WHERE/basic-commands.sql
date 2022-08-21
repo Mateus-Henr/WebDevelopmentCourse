ebe1ab20c7143ce4707db5cbfc4b937b5720b65f
Mateus-Henr <mateush.study@gmail.com>
2022-08-21 10:13:57 -0300

    Add SQL commands - READ

---
+++ Section 25 - SQL/SQL Commands - READ, SELECT, and WHERE/basic-commands.sql
@@ -0,0 +1,10 @@
+-- Reading data from db
+    SELECT * FROM "products"
+
+-- Getting specific columns
+    SELECT name, price FROM "products"
+
+-- Getting specific row
+    SELECT * FROM "products" WHERE id = 1
+
+-- There are other logical operators for narrowing down data.
