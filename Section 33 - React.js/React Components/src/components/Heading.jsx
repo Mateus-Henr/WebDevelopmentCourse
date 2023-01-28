309f2f9ca190a3bce948e9cd2e48a82356851be1
Matt <mateus.h.figueiredo@ufv.br>
2023-01-28 12:37:34 -0300

    Add info on React components

---
+++ Section 33 - React.js/React Components/src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// This is a component. Components have to start with a capital letter (Pascal case). It differentiates from the HTML
+// elements.
+function Heading()
+{
+    return <h1>My Favourite Foods</h1>;
+}
+
+// Always export the component.
+export default Heading;
